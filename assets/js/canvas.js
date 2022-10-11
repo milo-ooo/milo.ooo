61648abb2970cf829a6688bd57b8460d7de5901c
Mathilde Blanchemanche <mathilde.blanchemanche@xwiki.com>
2022-10-11 14:01:37 +0200

    prettier the code

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,22 +1,22 @@
 var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    var canvas = document.getElementsByTagName('canvas')[0];
-   
-    // adapter le canvas à la fenêtre
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
+var ctx = c.getContext("2d");
+var canvas = document.getElementsByTagName("canvas")[0];
 
-    // afficher l'image
-    var i = 1;
+// adapter le canvas à la fenêtre
+c.width = window.innerWidth;
+c.height = window.innerHeight;
 
-    function draw(event) {
-      var mousex = event.clientX;
-      var mousey = event.clientY;
-      var img = document.getElementById("image_0" + i);
-      ctx.drawImage(img, mousex - (img.width / 4) / 2, mousey - (img.height / 4) / 2, (img.width / 4), (img.height / 4));
-      if (i < 4) {
-        i++;
-      } else {
-        i = 1;
-      }
-    }
+// afficher l'image
+var i = 1;
+
+function draw(event) {
+  var mousex = event.clientX;
+  var mousey = event.clientY;
+  var img = document.getElementById("image_0" + i);
+  ctx.drawImage(img, mousex - img.width / 4 / 2, mousey - img.height / 4 / 2, img.width / 4, img.height / 4);
+  if (i < 4) {
+    i++;
+  } else {
+    i = 1;
+  }
+}
